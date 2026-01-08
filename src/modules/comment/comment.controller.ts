e3f449f18373e0887824651455ab45ebeaebc566
Al-Amin Islam <jimalamin7@gmail.com>
2026-01-08 23:54:58 +0600

    comment create

---
+++ src/modules/comment/comment.controller.ts
@@ -0,0 +1,22 @@
+import { Request, Response } from "express";
+import { commentService } from "./comment.service";
+
+const createComment = async (req: Request, res: Response) => {
+  try {
+    const user = req.user;
+    req.body.authorId = user?.id;
+
+    const result = await commentService.createComment(req.body);
+    console.log(req.body);
+    res.status(201).json(result);
+  } catch (error: any) {
+    res.status(400).json({
+      error: "comment creation failed",
+      details: error,
+    });
+  }
+};
+
+export const commentController = {
+  createComment,
+};
--- src/modules/comment/comment.route.ts
+++ src/modules/comment/comment.route.ts
@@ -1,5 +1,14 @@
 import express from "express";
+import { commentController } from "./comment.controller";
+import auth from "../middleware/auth";
+import { UserRole } from "../../types/type";
 
 const router = express.Router();
+
+router.post(
+  "/",
+  auth(UserRole.USER, UserRole.ADMIN),
+  commentController.createComment
+);
 
 export const commentRouter = router;
