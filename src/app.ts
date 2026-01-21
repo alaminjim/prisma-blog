a9cfac701f07cdedc1b3a867a06e93008a6de0b9
Al-Amin Islam <jimalamin7@gmail.com>
2026-01-21 22:17:41 +0600

    not found error handle

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { toNodeHandler } from "better-auth/node";
 import cors from "cors";
 import { commentRouter } from "./modules/comment/comment.route";
 import errorHandler from "./modules/middleware/errorHandler";
+import { notFound } from "./modules/middleware/notFound";
 
 const app = express();
 
@@ -25,6 +26,8 @@ app.all("/api/auth/*splat", toNodeHandler(auth));
 app.get("/", (req: Request, res: Response) => {
   res.send("API IS WORKING");
 });
+
+app.use(notFound);
 
 app.use(errorHandler);
 
