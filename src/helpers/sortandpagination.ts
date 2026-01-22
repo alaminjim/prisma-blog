6a35e0e7d9725d0bcc8ecd0aad5009d9991b05ca
Al-Amin Islam <jimalamin7@gmail.com>
2026-01-22 23:57:20 +0600

    edit helper

--- src/helpers/sortandpagination.ts
+++ src/helpers/sortandpagination.ts
@@ -5,7 +5,13 @@ type IOptions = {
   sortOrder?: string;
 };
 
+const sortAndPagination = (options: IOptions) => {
+  const page: number = Number(options.page) || 1;
+  const limit: number = Number(options.limit) || 5;
+  const skip = (page - 1) * limit;
 
+  const sortBy: string = options.sortBy || "createdAt";
+  const sortOrder: string = options.sortOrder || "desc";
 
   return {
     page,
