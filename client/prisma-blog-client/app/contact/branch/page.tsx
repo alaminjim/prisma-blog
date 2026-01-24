e3fb755083ac330ae9a8b3911497a1df24f58cb1
Al-Amin Islam <jimalamin7@gmail.com>
2026-01-24 20:55:48 +0600

    basic route

---
+++ client/prisma-blog-client/app/contact/branch/page.tsx
@@ -0,0 +1,7 @@
+export default function BranchPage() {
+  return (
+    <div>
+      <h1>this is a brach page</h1>
+    </div>
+  );
+}
---
+++ client/prisma-blog-client/app/contact/layout.tsx
@@ -0,0 +1,12 @@
+export default function ContactLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <h1>this is a contact layout</h1>
+      {children}
+    </div>
+  );
+}
